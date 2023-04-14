99305c35fe0641eef616230860868446461041e7
VictorMartinan <117628459+VictorMartinan@users.noreply.github.com>
2023-04-14 13:46:11 +0200

    Update enemy.py

--- Class/enemy.py
+++ Class/enemy.py
@@ -1,10 +1,19 @@
-class Enemy:
-    def __init__(self, x, y):
-        self.health = 50
+class Enemy():
+    
+    def __init__(self,health, speed, x, y, size, displayWidth, displayHeight, score, image):
+        self.health = health
+        self.speed = speed
         self.x = x
         self.y = y
-        self.width = 150
-        self.height = 150
+        self.size = size
+        self.displayWidth = displayWidth
+        self.displayHeight = displayHeight
+        self.score = score
+        self.image = image
+
+    def move(self, veloX, veloY):
+        self.x = self.x + veloX * self.speed
+        self.y = self.y + veloY * self.speed
 
     def takeDmg(self, dmg):
         self.health -= dmg
