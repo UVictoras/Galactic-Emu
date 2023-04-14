c3eaef06b56f676f1a9c521c79ea140648396da0
VictorMartinan <117628459+VictorMartinan@users.noreply.github.com>
2023-04-14 13:45:08 +0200

    Update player.py

--- Class/player.py
+++ Class/player.py
@@ -1,17 +1,24 @@
+import math
+
 class Player:
-    X = 0
-    Y = 0
-    def __init__(self, basicSpeed, slowSpeed, size, img, displayWidth, displayHeight, dashSpeed,cooldownDash,timeDash)
+
+    def __init__(self, basicSpeed, slowSpeed, size, img, displayWidth, displayHeight, dashSpeed,cooldownDash,timeDash, health):
+        self.X = 0
+        self.Y = 0
         self.basicSpeed = basicSpeed
         self.slowSpeed = slowSpeed
         self.speed = basicSpeed
         self.size = size
-        self.img = pygame.transform.scale(img, (self.size, self.size))
+        self.img = img
+        self.displayWidth = displayWidth
+        self.displayHeight = displayHeight
         self.dashSpeed = dashSpeed
         self.cooldownDash = cooldownDash
         self.timeDash = timeDash
+        self.health = health
 
     def move(self, veloX, veloY):
+
         if veloX != 0 and veloY != 0:
             self.X = self.X + math.sqrt(1/2) * self.speed
             self.Y = self.Y + math.sqrt(1/2) * self.speed
@@ -20,11 +27,14 @@ class Player:
             self.Y = self.Y + veloY * self.speed
         
         #Stop player from going out of the screen
-        if self.X > displayWidth - self.size:
-            self.X = displayWidth - self.size
+        if self.X > self.displayWidth - self.size:
+            self.X = self.displayWidth - self.size
         if self.X < 0:
             self.X = 0
-        if self.Y > displayHeight - self.size:
-            self.Y = displayHeight - self.size
+        if self.Y > self.displayHeight - self.size:
+            self.Y = self.displayHeight - self.size
         if self.Y < 0:
             self.Y = 0
+    
+    def takeDmg(self, dmg):
+        self.health -= dmg
