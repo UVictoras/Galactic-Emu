155fba5dba6d426db16bcca6e36c0724c04bb319
Victoraş <117628459+VictorMartinan@users.noreply.github.com>
2023-04-15 21:42:34 +0200

    Update bulletHandler.py

--- Class/bulletHandler.py
+++ Class/bulletHandler.py
@@ -1,7 +1,6 @@
 import math
 import pygame
 from Class.projectile import Projectile
-
 class BulletHandler():
     maxSpinSpeed=0
     radius=100
@@ -9,25 +8,31 @@ class BulletHandler():
     bulletCurve=0
     invertSpin=False
     bulletTTL = 0
+    X = 0
+    Y = 0
 
-    def __init__(self, firingSpeed, arrayNumber, angleBetweenArrays):
+    def __init__(self, firingSpeed, arrayNumber, angleBetweenArrays, projectileList, img, isHoming = False, isPlayer = False):
         self.firingSpeed = firingSpeed
         self.arrayNumber = arrayNumber
         self.angleBetweenArrays = angleBetweenArrays
         self.arrayList = []
-        arrayX = 100
-        arrayY = 100
-        for array in range(self.arrayNumber):
-            array = (arrayX,arrayY)
-            self.arrayList.append(array)
-            arrayX = math.cos(self.angleBetweenArrays) - math.sin(self.angleBetweenArrays)
-            arrayY = math.sin(self.angleBetweenArrays) + math.cos(self.angleBetweenArrays)
+        self.projectileList = projectileList
+        self.img = img
+        self.isPlayer = isPlayer
+        self.isHoming = isHoming
+
 
     def update(self):
-        print("Update")
-        #Import missile model
-        missile = pygame.image.load("img/missile.png")
-        missile = pygame.transform.scale(missile, (50, 50))
-        missileWidth = missile.get_width()
-        for array in self.arrayList:
-            bullet = Projectile(array[0], array[1],missileWidth,missile)
+        #Import bullet image
+        bulletWidth = self.img.get_width()
+        #create bullet
+        if self.isPlayer:
+            velocity = pygame.math.Vector2(0,-50)
+        else:
+            velocity = pygame.math.Vector2(0,10)
+        bullet = Projectile(self.X, self.Y+10, bulletWidth, self.img, velocity, 5, self.isHoming, 1920, 1080, self.projectileList, self.isPlayer)
+
+    def move(self, targetX, targetY):
+        '''Updates the position of the bullet handler'''
+        self.X = targetX
+        self.Y = targetY
