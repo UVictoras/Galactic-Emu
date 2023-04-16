733cd56a241982ce92ba7ceb927dcfa7ab7a9182
Coucoul38 <noham.coulon@gmail.com>
2023-04-16 11:20:43 +0200

    merge

--- Class/bulletHandler.py
+++ Class/bulletHandler.py
@@ -11,13 +11,8 @@ class BulletHandler():
     X = 0
     Y = 0
 
-<<<<<<< HEAD
     def __init__(self, bulletSpeed, arrayNumber, angleBetweenArrays, projectileList, img, isHoming = False, isPlayer = False):
         self.bulletSpeed = bulletSpeed
-=======
-    def __init__(self, firingSpeed, arrayNumber, angleBetweenArrays, projectileList, img, isHoming = False, isPlayer = False):
-        self.firingSpeed = firingSpeed
->>>>>>> b1e51fd8cf0ef78ceee913807d0509652e959c2e
         self.arrayNumber = arrayNumber
         self.angleBetweenArrays = angleBetweenArrays
         self.arrayList = []
@@ -25,10 +20,6 @@ class BulletHandler():
         self.img = img
         self.isPlayer = isPlayer
         self.isHoming = isHoming
-<<<<<<< HEAD
-=======
-
->>>>>>> b1e51fd8cf0ef78ceee913807d0509652e959c2e
 
     
     def update(self):
@@ -36,23 +27,12 @@ class BulletHandler():
         bulletWidth = self.img.get_width()
         #create bullet
         if self.isPlayer:
-<<<<<<< HEAD
             velocity = pygame.math.Vector2(0,-1)
         else:
             velocity = pygame.math.Vector2(0,1)
         bullet = Projectile(self.X, self.Y, bulletWidth, self.img, velocity, 5, self.isHoming, 1920, 1080, self.projectileList, self.bulletSpeed, self.isPlayer)
-=======
-            velocity = pygame.math.Vector2(0,-50)
-        else:
-            velocity = pygame.math.Vector2(0,10)
-        bullet = Projectile(self.X, self.Y+10, bulletWidth, self.img, velocity, 5, self.isHoming, 1920, 1080, self.projectileList, self.isPlayer)
->>>>>>> b1e51fd8cf0ef78ceee913807d0509652e959c2e
 
     def move(self, targetX, targetY):
         '''Updates the position of the bullet handler'''
         self.X = targetX
-<<<<<<< HEAD
         self.Y = targetY
-=======
-        self.Y = targetY
->>>>>>> b1e51fd8cf0ef78ceee913807d0509652e959c2e
