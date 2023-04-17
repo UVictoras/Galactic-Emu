e44321a29172e18dc22aebb399b259f34b675bab
VictorMartinan <vmartinan@gaming.tech>
2023-04-17 14:01:51 +0200

    speedrun de Noham

--- Class/bulletHandler.py
+++ Class/bulletHandler.py
@@ -22,15 +22,23 @@ class BulletHandler():
         self.isHoming = isHoming
 
     
-    def update(self):
+    def update(self, direction):
         #Import bullet image
         bulletWidth = self.img.get_width()
-        #create bullet
-        if self.isPlayer:
-            velocity = pygame.math.Vector2(0,-1)
-        else:
-            velocity = pygame.math.Vector2(0,1)
-        bullet = Projectile(self.X, self.Y, bulletWidth, self.img, velocity, 5, self.isHoming, 1920, 1080, self.projectileList, self.bulletSpeed, self.isPlayer)
+        numNegatives = math.floor(self.arrayNumber / 2)
+        numPositives = math.ceil(self.arrayNumber / 2)
+        arrayMax = numPositives - 1
+        arrayMin = -numNegatives
+
+        for array in range(arrayMin, arrayMax+1):
+            #create bullet
+            directionAngle = math.atan2(direction[1], direction[0])
+
+            radians = math.radians(array * self.angleBetweenArrays) + directionAngle
+            destX = math.cos(radians)
+            destY = math.sin(radians)
+            arrayDirection = (destX, destY)
+            bullet = Projectile(self.X, self.Y, bulletWidth, self.img, arrayDirection, 1, self.isHoming, 1920, 1080, self.projectileList, self.bulletSpeed, self.isPlayer)
 
     def move(self, targetX, targetY):
         '''Updates the position of the bullet handler'''
--- main.py
+++ main.py
@@ -72,9 +72,9 @@ player = Player(10, 5, 50, displayWidth, displayHeight, 30, 60, 15, 5, projectil
 imgEnemy = pygame.image.load("img/enemy.png").convert()
 imgEnemy = pygame.transform.scale(imgEnemy, (50, 50))
 
-enemy1 = Enemy(50, 2, 300, 0, 50, displayWidth, displayHeight, 100, imgEnemy, 10, 4, math.pi/2, projectileList, 1, "left")
-enemy2 = Enemy(50, 2, 1200, 0, 50, displayWidth, displayHeight, 100, imgEnemy, 10, 4, math.pi/2, projectileList, 1, "left")
-enemy3 = Enemy(50, 2, 500, 0, 50, displayWidth, displayHeight, 100, imgEnemy, 10, 4, math.pi/2, projectileList, 1, "left")
+enemy1 = Enemy(50, 2, 300, 0, 50, displayWidth, displayHeight, 100, imgEnemy, 4, 10, 5, projectileList, 1, "left")
+enemy2 = Enemy(50, 2, 1200, 0, 50, displayWidth, displayHeight, 100, imgEnemy, 10, 3, 10, projectileList, 1, "left")
+enemy3 = Enemy(50, 2, 500, 0, 50, displayWidth, displayHeight, 100, imgEnemy, 10, 3, 10, projectileList, 1, "left")
 enemyList = [enemy1, enemy2, enemy3]
 
 
@@ -166,7 +166,7 @@ while running:
 
     #Enemy
     for enemy in enemyList:
-        enemy.update()
+        enemy.update(player)
         rect = pygame.Rect(enemy.x, enemy.y, enemy.size, enemy.size)
         
         screen.blit(enemy.image, (enemy.x, enemy.y))
